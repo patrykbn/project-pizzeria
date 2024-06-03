957eda1c9cd0aba637c9ddfc95a079ab6c1db9d3
Patryk Bagan <patrykbn1997@gmail.com>
2024-06-03 13:18:12 +0200

    MainPage.js bugfix & eslint ecmaVer changed

--- dist/js/components/mainPage.js
+++ dist/js/components/mainPage.js
@@ -3,40 +3,43 @@ import {templates, settings} from "../settings.js";
 
 class mainPage{
     constructor(element){
-        const thisMainPage = this;
+        //const thisMainPage = this;
 
-        thisMainPage.renderMainPage(element);
+        this.renderMainPage(element);
     }
 
     async renderMainPage(element) {
-        const thisMainPage = this;
         const url = `${settings.db.url}/${settings.db.homePage}`;
         console.log('Fetching data from URL:', url);
-        
-        
-        const response = await fetch(url);
-        
-        const data = await response.json();
-        console.log('Fetched data:', data);
 
-        const homePageData = data[0];
-        const generatedHTML = templates.homePage(homePageData);
+        try {
+            const response = await fetch(url);
+            const data = await response.json();
+            console.log('Fetched data:', data);
 
-        thisMainPage.dom = {};
-        thisMainPage.dom.wrapper = element;
-        thisMainPage.dom.wrapper.innerHTML = generatedHTML;
+            const homePageData = data[0];
+            const generatedHTML = templates.homePage(homePageData);
 
-        thisMainPage.rednerCarousel();
+            this.dom = {};
+            this.dom.wrapper = element;
+            this.dom.wrapper.innerHTML = generatedHTML;
+
+            this.renderCarousel();
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
     }
-    rednerCarousel(){
-      const thisMainPage = this;
+    
+    renderCarousel(){
       var elem = document.querySelector('.main-carousel');
+      // eslint-disable-next-line no-undef
       var flkty = new Flickity( elem, {
-      autoPlay: 'true',
-      reset: 'true',
+      autoPlay: true,
+      wrapAround: true,
       cellAlign: 'left',
       contain: true
     });
+    console.log(flkty);
     }
 }
 
